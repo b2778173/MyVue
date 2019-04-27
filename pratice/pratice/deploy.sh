b933cc5fa25e10405b7a77a001dc1ea603be6e0e
Kevin Tsai <b2778173@hotmail.com>
2019-04-27 21:04:29 +0800

    Create deploy.sh

---
+++ pratice/pratice/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://b2778173.github.io
+# git push -f git@github.com:b2778173/b2778173.github.io.git master
+
+# if you are deploying to https://b2778173.github.io/test
+git push -f https://github.com/b2778173/MyVue.git master:gh-pages
+
+cd -
